e644b175d7a6dba0a280ddc2b63b8c9afeca82b7
Vlada Didko <slava.didko@gmail.com>
2017-12-15 04:34:38 +0200

    d

---
+++ Frontend/WorksList.js
@@ -0,0 +1,185 @@
+var works_info = [
+    {
+        id:1,
+        icon:'assets/images/gallery/creature/cr1.jpg',
+        type: 'Concept art of creatures',
+    },
+
+    {
+        id:2,
+        icon:'assets/images/gallery/creature/cr2.jpg',
+        type: 'Concept art of creatures',
+    },
+    {
+        id:3,
+        icon:'assets/images/gallery/creature/cr3.jpg',
+        type: 'Concept art of creatures',
+    },
+    {
+        id:4,
+        icon:'assets/images/gallery/creature/cr4.jpg',
+        type: 'Concept art of creatures',
+    },
+    {
+        id:5,
+        icon:'assets/images/gallery/creature/cr5.jpg',
+        type: 'Concept art of creatures',
+    },
+    {
+        id:6,
+        icon:'assets/images/gallery/creature/cr6.jpg',
+        type: 'Concept art of creatures',
+    },
+    {
+        id:11,
+        icon:'assets/images/gallery/environment/en1_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:12,
+        icon:'assets/images/gallery/environment/en2_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:13,
+        icon:'assets/images/gallery/environment/en3_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:14,
+        icon:'assets/images/gallery/environment/en4_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:15,
+        icon:'assets/images/gallery/environment/en5_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:16,
+        icon:'assets/images/gallery/environment/en6_s.jpg',
+        type:'Concept of environment',
+    },
+    {
+        id:21,
+        icon:'assets/images/gallery/character/ch1.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:22,
+        icon:'assets/images/gallery/character/ch2.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:23,
+        icon:'assets/images/gallery/character/ch3.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:24,
+        icon:'assets/images/gallery/character/ch4.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:25,
+        icon:'assets/images/gallery/character/ch5.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:26,
+        icon:'assets/images/gallery/character/ch6.jpg',
+        type:'Concept art of characters',
+    },
+    {
+        id:31,
+        icon:'assets/images/gallery/architecture/ar1.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:32,
+        icon:'assets/images/gallery/architecture/ar2.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:33,
+        icon:'assets/images/gallery/architecture/ar3.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:34,
+        icon:'assets/images/gallery/architecture/ar4.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:35,
+        icon:'assets/images/gallery/architecture/ar5.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:36,
+        icon:'assets/images/gallery/architecture/ar6.jpg',
+        type:'Concept art of architecture',
+    },
+    {
+        id:41,
+        icon:'assets/images/gallery/3D/3d1.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:42,
+        icon:'assets/images/gallery/3D/3d2.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:43,
+        icon:'assets/images/gallery/3D/3d3.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:44,
+        icon:'assets/images/gallery/3D/3d4.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:45,
+        icon:'assets/images/gallery/3D/3d5.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:46,
+        icon:'assets/images/gallery/3D/3d6.jpg',
+        type:'3D modeling',
+    },
+    {
+        id:51,
+        icon:'assets/images/gallery/illustration/il1.jpg',
+        type:'Illustration',
+    },
+    {
+        id:52,
+        icon:'assets/images/gallery/illustration/il2.jpg',
+        type:'Illustration',
+    },
+    {
+        id:53,
+        icon:'assets/images/gallery/illustration/il3.jpg',
+        type:'Illustration',
+    },
+    {
+        id:54,
+        icon:'assets/images/gallery/illustration/il4.jpg',
+        type:'Illustration',
+    },
+    {
+        id:55,
+        icon:'assets/images/gallery/illustration/il5.jpg',
+        type:'Illustration',
+    },
+    {
+        id:56,
+        icon:'assets/images/gallery/illustration/il6.jpg',
+        type:'Illustration',
+    }
+    
+];
+module.exports = works_info;
